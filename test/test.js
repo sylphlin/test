c49ab58585bf4ca6fdbb625d060053ea4e447722
root <sylph.lin@gmail.com>
2015-05-25 10:29:57 +0000

    Modify Test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-require('mocha-jscs')(['./test', './models']); // Array of paths
+// require('mocha-jscs')(['./test', './models']); // Array of paths
 
 var assert = require('chai').assert;
 var should = require('chai').should();
@@ -6,10 +6,10 @@ var expect = require('chai').expect();
 var greetings = require('../libs/greetings');
 var keygen = require('../libs/keygen');
 
-describe('Keygen', function(){
-  describe('generate("12345")', function(){
-    it('should return 827CCB0EEA8A706C4C34A16891F84E7B', function(){
-      keygen.generate("12345").should.equal('827CCB0EEA8A706C4C34A16891F84E7B');
-    })
-  })
-})
+describe('Keygen', function() {
+  describe('generate("12345")', function() {
+    it('should return 1827CCB0EEA8A706C4C34A16891F84E7B', function() {
+      keygen.generate('12345').should.equal('1827CCB0EEA8A706C4C34A16891F84E7B');
+    });
+  });
+});
